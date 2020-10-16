074d2608be083f8ad12864a972b1acc4ff9e08d9
Ashok Bakthavathsalam <kgashok@kgkite.ac.in>
2020-10-16 08:17:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+
+f = open("hype.csv")
+
+content = f.read()
+
+#print(content)
+lines = content.split("\n")
+#print(len(lines))
+
+#print(lines[0])
+
+col = lines[0].split(",")
+#print(len(col))
+
+db = {}
+
+for line in lines[1:280]: 
+  columns = line.split(",")
+  if len(columns) == 78: 
+    #print(columns)
+    tech = columns[1]
+    category = columns[-1]
+    #print(tech, category)
+
+    if len(category) and category not in db:
+      db[category] = []
+    if len(tech): 
+      db[category].append(tech)
+
+print(len(db))
+from pprint import pprint
+
+pprint(db)
+
+with open('output.txt', 'wt') as out:
+    pprint(db, stream=out)
